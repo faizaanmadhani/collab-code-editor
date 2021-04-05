69c60e010a788b73b9afe7b07b95b97821508c7e
Faizaan Madhani <fzmadhani@gmail.com>
2021-04-04 23:24:38 -0400

    Remove stuff

--- mobile/README.md
+++
@@ -1,3 +0,0 @@
-# reading-list
-
-A Dynamic Reading List Application that recommends books for you to read, based on your current list.
--- server/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { ReadingListServer } from "./server"
-
-const port = "3030"
-const server = new ReadingListServer();
-server.start(port);
